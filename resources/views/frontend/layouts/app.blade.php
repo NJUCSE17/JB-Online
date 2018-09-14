298c9db19cf9f3c4091257a76d46603a96e49f05
Tianyun Zhang <doowzs@qq.com>
2018-09-14 19:40:19 +0800

    +SVG backgrounds

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -137,7 +137,7 @@
 
         @stack('after-styles')
     </head>
-    <body style="background-image: url('{{ asset('images/frontend/background/bg_f_'.random_int(1, 14).'.svg')}}');
+    <body style="background-image: url('{{ asset('images/frontend/background/bg_'.random_int(1, 20).'.svg')}}');
             background-size: auto; background-position: center center;">
         @include('includes.partials.logged-in-as')
         @include('frontend.includes.nav')
