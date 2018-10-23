b371db0b0c2efab9f885f3bceaae858553de96a7
Tianyun Zhang <doowzs@qq.com>
2018-10-23 23:41:03 +0800

    disable scaling on mobile devices

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -6,7 +6,7 @@
 @endlangrtl
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', app_name())</title>
     <meta name="description" content="@yield('meta_description', 'Homework Forum Backend')">
