4dafd8deeb5ec8f6e67c78979eae67e75e6760be
Tianyun Zhang <doowzs@qq.com>
2018-11-20 02:22:54 +0800

    remove mcrypt for PHP7.2 in testing

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -9,7 +9,7 @@ apt-get update -yqq
 apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
 
 # Compile PHP, include these extensions.
-docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
+docker-php-ext-install mbstring pdo_mysql curl json intl gd xml zip bz2 opcache
 
 # Install Composer and project dependencies.
 curl -sS https://getcomposer.org/installer | php
