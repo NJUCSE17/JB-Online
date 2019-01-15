0dbc93399cc2258f6eccd62468855f0879f8c013
Tianyun Zhang <doowzs@qq.com>
2019-01-16 00:55:38 +0800

    allow table header in purifier

--- config/purifier.php
+++ config/purifier.php
@@ -24,7 +24,7 @@ return [
     'settings'      => [
         'default' => [
             'HTML.Doctype'             => 'HTML 4.01 Transitional',
-            'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,dl,dt,dd,p[style],br,span[style],img[width|height|alt|src],pre,code,figure,table,td,tr,h1,h2,h3,h4,h5,h6,blockquote',
+            'HTML.Allowed'             => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,dl,dt,dd,p[style],br,span[style],img[width|height|alt|src],pre,code,figure,table,th,td,tr,h1,h2,h3,h4,h5,h6,blockquote',
             'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
             'AutoFormat.AutoParagraph' => true,
             'AutoFormat.RemoveEmpty'   => true,
