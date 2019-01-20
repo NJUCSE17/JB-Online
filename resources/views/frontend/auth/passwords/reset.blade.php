9f522452526288f667d350eadac79f98e14fd531
Tianyun Zhang <doowzs@qq.com>
2019-01-20 16:45:01 +0800

    fix page layout on password reset page

--- resources/views/frontend/auth/passwords/reset.blade.php
+++ resources/views/frontend/auth/passwords/reset.blade.php
@@ -23,58 +23,59 @@
                     @endif
 
                     {{ html()->form('POST', route('frontend.auth.password.reset'))->class('form-horizontal')->open() }}
-                        {{ html()->hidden('token', $token) }}
+                    {{ html()->hidden('token', $token) }}
 
-                        <div class="row mb-2">
-                            <div class="col">
-                                <div class="input-group input-group-seamless">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text"><i class="fas fa-envelope"></i></div>
-                                    </div>
-                                    {{ html()->email('email')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.email'))
-                                        ->attribute('maxlength', 191)
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                    <div class="row mb-2">
+                        <div class="col">
+                            <div class="input-group input-group-seamless">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+                                </div>
+                                {{ html()->email('email')
+                                    ->class('form-control')
+                                    ->placeholder(__('validation.attributes.frontend.email'))
+                                    ->attribute('maxlength', 191)
+                                    ->required() }}
+                            </div><!--form-group-->
+                        </div><!--col-->
+                    </div><!--row-->
 
-                        <div class="row mb-2">
-                            <div class="col">
-                                <div class="input-group input-group-seamless">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text"><i class="fas fa-key"></i></div>
-                                    </div>
-                                    {{ html()->password('password')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.password'))
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                    <div class="row mb-2">
+                        <div class="col">
+                            <div class="input-group input-group-seamless">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text"><i class="fas fa-key"></i></div>
+                                </div>
+                                {{ html()->password('password')
+                                    ->class('form-control')
+                                    ->placeholder(__('validation.attributes.frontend.password'))
+                                    ->required() }}
+                            </div><!--form-group-->
+                        </div><!--col-->
+                    </div><!--row-->
 
-                        <div class="row mb-2">
+                    <div class="row mb-2">
+                        <div class="col">
                             <div class="input-group input-group-seamless">
                                 <div class="input-group-prepend">
                                     <div class="input-group-text"><i class="fas fa-redo"></i></div>
                                 </div>
-                                    {{ html()->password('password_confirmation')
-                                        ->class('form-control')
-                                        ->placeholder(__('validation.attributes.frontend.password_confirmation'))
-                                        ->required() }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                                {{ html()->password('password_confirmation')
+                                    ->class('form-control')
+                                    ->placeholder(__('validation.attributes.frontend.password_confirmation'))
+                                    ->required() }}
+                            </div><!--form-group-->
+                        </div><!--col-->
+                    </div><!--row-->
 
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group mb-0 clearfix text-right">
-                                    {{ form_submit("<i class='fas fa-unlock mr-2'></i>"
-                                        . __('labels.frontend.passwords.reset_password_button')) }}
-                                </div><!--form-group-->
-                            </div><!--col-->
-                        </div><!--row-->
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group mb-0 clearfix text-right">
+                                {{ form_submit("<i class='fas fa-unlock mr-2'></i>"
+                                    . __('labels.frontend.passwords.reset_password_button')) }}
+                            </div><!--form-group-->
+                        </div><!--col-->
+                    </div><!--row-->
                     {{ html()->form()->close() }}
                 </div><!-- card-body -->
             </div><!-- card -->
