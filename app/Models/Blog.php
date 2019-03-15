ad45cd1b10c331319120243b599c8aa3a998141e
Tianyun Zhang <doowzs@qq.com>
2019-03-15 21:21:00 +0800

    add blog model

---
+++ app/Models/Blog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content_html',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+}
