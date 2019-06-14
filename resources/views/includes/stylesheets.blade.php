d2375021947f3601f46e697e7793aa126ea081dc
Tianyun Zhang <doowzs@qq.com>
2019-06-14 18:23:42 +0800

    update composer.json

--- resources/views/includes/stylesheets.blade.php
+++ resources/views/includes/stylesheets.blade.php
@@ -1,3 +1,3 @@
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('css/fa.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('css/purpose.css') }}" rel="stylesheet" type="text/css">
