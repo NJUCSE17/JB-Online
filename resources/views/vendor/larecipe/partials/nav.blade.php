4adedba93bc62cae7b92132f31e68bf2367d8abe
Tianyun Zhang <doowzs@qq.com>
2019-06-15 22:08:22 +0800

    update manual - GitHub link and icon

--- resources/views/vendor/larecipe/partials/nav.blade.php
+++ resources/views/vendor/larecipe/partials/nav.blade.php
@@ -4,10 +4,8 @@
     <nav class="flex items-center justify-between text-black bg-navbar shadow-xs h-16">
         <div class="flex items-center flex-no-shrink">
             <a href="{{ url('/') }}" class="flex items-center flex-no-shrink text-black mx-4">
-                @include("larecipe::partials.logo")
-
                 <p class="inline-block font-semibold mx-1 text-grey-dark">
-                    {{ config('app.name') }}
+                    {{ config('app.name') }} - Docs
                 </p>
             </a>
 
@@ -27,7 +25,7 @@
                 </larecipe-button>
             @endif
 
-            <larecipe-button tag="a" href="https://github.com/saleem-hadad/larecipe" target="__blank" type="black" class="mx-2 px-4">
+            <larecipe-button tag="a" href="https://github.com/doowzs/JB-Online-Remaster" target="__blank" type="black" class="mx-2 px-4">
                 <i class="fab fa-github"></i>
             </larecipe-button>
 
