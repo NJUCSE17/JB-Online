4b56676991c4be02bea868f68aad93ef765ceef7
Tianyun Zhang <doowzs@qq.com>
2019-06-23 00:24:01 +0800

    cast user want_email property as boolean

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,6 +63,7 @@ class User extends Authenticatable
      */
     protected $casts
         = [
+            'want_email'        => 'boolean',
             'email_verified_at' => 'datetime',
             'activated_at'      => 'datetime',
             'last_login_at'     => 'datetime',
