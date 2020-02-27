ad0620efa3096c226d88f3e575942553158d9ab1
Tianyun Zhang <doowzs@qq.com>
2020-02-27 12:18:57 +0000

    add icons for 4xx snow weather

--- resources/js/components/Weather/WeatherIcon.vue
+++ resources/js/components/Weather/WeatherIcon.vue
@@ -17,6 +17,14 @@
     <i v-else-if="code === '309'" class="fas mr-1 fa-cloud-drizzle"></i>
     <i v-else-if="code === '310'" class="fas mr-1 fa-cloud-showers-heavy"></i>
     <i v-else-if="code === '399'" class="fas mr-1 fa-cloud-rain"></i>
+    <i v-else-if="code === '400'" class="fas mr-1 fa-snowflake"></i>
+    <i v-else-if="code === '401'" class="fas mr-1 fa-snowflake"></i>
+    <i v-else-if="code === '402'" class="fas mr-1 fa-snowflake"></i>
+    <i v-else-if="code === '403'" class="fas mr-1 fa-snowflake"></i>
+    <i v-else-if="code === '404'" class="fas mr-1 fa-cloud-snow"></i>
+    <i v-else-if="code === '405'" class="fas mr-1 fa-cloud-snow"></i>
+    <i v-else-if="code === '406'" class="fas mr-1 fa-cloud-snow"></i>
+    <i v-else-if="code === '499'" class="fas mr-1 fa-snowflake"></i>
     <i v-else class="fas mr-1 fa-question-square"></i>
 </template>
 
